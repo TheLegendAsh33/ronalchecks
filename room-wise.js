66795867e8123abf688425a2434094718adab7cd
ash 1913 <ashishganeshshinde@gmail.com>
2025-07-04 11:33:46 +0530

    y

--- room-wise.js
+++ room-wise.js
@@ -51,7 +51,7 @@ function capturePhoto(room, feature, blockId) {
 
   const img = document.createElement("img");
   img.src = dataURL;
-  img.style.width = "50px";
+  img.style.width = "100px";
   img.style.height = "100px";
   img.style.margin = "5px";
   img.style.cursor = "pointer";
