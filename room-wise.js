789379bbc48e1e23a5b350f33cce4e5950975d98
ash 1913 <ashishganeshshinde@gmail.com>
2025-07-04 11:30:09 +0530

    updtae

--- room-wise.js
+++ room-wise.js
@@ -51,7 +51,8 @@ function capturePhoto(room, feature, blockId) {
 
   const img = document.createElement("img");
   img.src = dataURL;
-  img.style.width = "150px";
+  // img.style.width = "150px";
+  img.style.height = "200px";
   img.style.margin = "5px";
   img.style.cursor = "pointer";
   img.title = "Click to delete";
